dbf208431ddce160c7a2c863996ce12c8092d845
Admin <krystianbudzisz1@gmail.com>
2023-12-30 19:10:42 +0100

    Zajecia 30.12

--- src/main/java/com/example/emailservice/model/EmailDetails.java
+++ src/main/java/com/example/emailservice/model/EmailDetails.java
@@ -1,13 +1,12 @@
 package com.example.emailservice.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 
-@Getter
-@Setter
-@AllArgsConstructor
+@Data
 public class EmailDetails {
     private String recipient;
     private String subject;
